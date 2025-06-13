c2074f80ecdfed335d37b09754fde8b0560c84e6
Tanvi <tanvitater8@gmail.com>
2025-06-13 15:53:41 +0530

    Fortune game using list

---
+++ fortune2.py
@@ -0,0 +1,14 @@
+import random
+messages = [
+    "You will find great fortune today.",
+    "A new opportunity is on the horizon.",
+    "Expect the unexpected.",
+    "Your hard work will soon pay off.",
+    "A surprise gift is coming your way.",
+    "You will meet someone special soon.",
+    "Your creativity will shine brightly today.",
+    "A financial gain is in your future.",
+    "You will overcome a challenge with ease.",
+    "Happiness is just around the corner."
+]
+print(messages[random.randint(0, len(messages)-1)])
